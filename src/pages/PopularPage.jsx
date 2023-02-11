9c90d3308cfde701bdefcf22a9588ffc2b3587c5
Luis Miguel Bolaños Santana <luismysantana1998@gmail.com>
2023-02-11 20:17:41 +0000

    Movies cards list in development

--- src/pages/PopularPage.jsx
+++ src/pages/PopularPage.jsx
@@ -7,28 +7,41 @@ import "../styles/PopularPage.scss"
 const PopularPage = () => {
 
     const [ isLoading, setIsLoading ] = useState(false);
-    const [ filmsList, setFilmsList ] = useState([]);
+    const [ filmsList, setFilmsList ] = useState({});
 
     
     useEffect(() => {
         loadPopularFilmsList(setFilmsList, setIsLoading, 1);
         
     }, []);
-
-
+    
     return (
         <main className="popular_films_page">
             <h1 className="popular_films_title">Most popular films list</h1>
+
             {isLoading ? 
                 <Spinner />
             :
-                !filmsList ? (
+                !filmsList?.results?.length ? (
+                    // If filmsList doesn't exists or it's results are empty, we show a message
                     <div className="no_data_warning_container">
                         <h1 className="no_data_warning">No data was found</h1>
                         <h3>Please try again in later</h3>
                     </div>
                 ):(
-                    <p>The is data</p>
+                    // Else we show the movies list
+                    <>
+                        <div className="films_card_list">
+                            {filmsList.results.map(film => (
+                                <p 
+                                    key={film.id}
+                                >
+                                    {film.title}
+                                </p>
+                            ))}
+                        </div>
+                        <div>Pagination</div>
+                    </>
                 )
             }
         </main>
