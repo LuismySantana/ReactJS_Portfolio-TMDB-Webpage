c33995be086796a7731a5f2ad85cde089e2063cd
Luis Miguel Bolaños Santana <luismysantana1998@gmail.com>
2023-02-11 04:47:12 +0000

    Changed BrowserRouter to the React-Router-Dom 6 'old' version
    I wanted to try the new RRD routing version but it's conflictive with React Context so I replaced it with the usual one.

--- src/context/AuthenticationProvider.jsx
+++ src/context/AuthenticationProvider.jsx
@@ -1,10 +1,15 @@
 import { createContext, useEffect, useState } from "react";
+import { useNavigate } from "react-router-dom";
+
 
 const AuthenticationContext = createContext();
 
 
 // As this is only a test project, we will be creating a context to simulate an user login system (creating a false session via localStorage)
 const AuthenticationProvider = ({children}) => {
+
+    // To navigate to home page on logout
+    const navigate = useNavigate();
 
     // State to check the the initial timing in which the session is checking the log state
     const [ isLogging, setIsLogging ] = useState(true);
@@ -56,6 +61,7 @@ const AuthenticationProvider = ({children}) => {
     function userLogout() {
         localStorage.removeItem("user_session_log");
         setAuthentication({});
+        navigate("/");
     }
     
     
--- src/main.jsx
+++ src/main.jsx
@@ -1,14 +1,28 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import router from './router/router';
-import { RouterProvider } from 'react-router-dom';
+import { BrowserRouter, Routes, Route } from 'react-router-dom'
+
 import { AuthenticationProvider } from './context/AuthenticationProvider';
+import Layout from './Layout';
+import UserProfile from "./pages/UserProfile";
 
 
 ReactDOM.createRoot(document.getElementById('root')).render(
     <React.StrictMode>
-        <AuthenticationProvider>
-            <RouterProvider router={router} />
-        </AuthenticationProvider>
+        <BrowserRouter>
+            <AuthenticationProvider>
+                <Routes>
+                    <Route path='/' element={<Layout />}>
+
+                        <Route index element={<p>Index Page</p>} />
+                        <Route path="/popular" element={<p>Popular Page</p>} />
+                        <Route path="/information/:id" element={<p>Film information Page</p>} />
+                        <Route path="/profile" element={<UserProfile />} />
+                        <Route path="/*" element={<p>404 Page</p>} />
+
+                    </Route>
+                </Routes>
+            </AuthenticationProvider>
+        </BrowserRouter>
     </React.StrictMode>
 )
