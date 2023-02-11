4c0980f930be9b8f17d97301ccc6760ff314dd74
Luis Miguel Bolaños Santana <luismysantana1998@gmail.com>
2023-02-11 23:51:48 +0000

    Pagination added

--- src/pages/PopularPage.jsx
+++ src/pages/PopularPage.jsx
@@ -2,20 +2,23 @@ import { useEffect, useState } from "react";
 import Spinner from "../components/Spinner";
 import { loadPopularFilmsList } from "../utils";
 import "../styles/PopularPage.scss"
+import "../styles/Pagination.scss"
 import MovieCard from "../components/MovieCard";
-
+import ReactPaginate from "react-paginate";
 
 const PopularPage = () => {
 
     const [ isLoading, setIsLoading ] = useState(false);
     const [ filmsList, setFilmsList ] = useState({});
+    const [ page, setPage ] = useState(1);
 
     
     useEffect(() => {
-        loadPopularFilmsList(setFilmsList, setIsLoading, 1);
+        loadPopularFilmsList(setFilmsList, setIsLoading, page);
         
-    }, []);
+    }, [page]);
     
+
     return (
         <main className="popular_films_page">
             <h1 className="popular_films_title">Most popular films list</h1>
@@ -31,26 +34,40 @@ const PopularPage = () => {
                     </div>
                 ):(
                     // Else we show the movies list
-                    <>
-                        <div className="films_card_list">
-                            {filmsList.results.map(film => {
-                                const { id, title, release_date, poster_path, popularity } = film;
-                                
-                                return <MovieCard
-                                    key={id}
-                                    id={id}
-                                    title={title}
-                                    release={release_date}
-                                    popularity={popularity}
-                                    imagePath={poster_path}
+                    <div className="films_card_list">
+                        {filmsList.results.map(film => {
+                            const { id, title, release_date, poster_path, popularity } = film;
+                            
+                            return <MovieCard
+                                key={id}
+                                id={id}
+                                title={title}
+                                release={release_date}
+                                popularity={popularity}
+                                imagePath={poster_path}
 
-                                />
-                            })}
-                        </div>
-                        <div>Pagination</div>
-                    </>
+                            />
+                        })}
+                    </div>
                 )
             }
+
+            {/* This module works by its own so it's better to place it outside the loading control */}
+            <ReactPaginate 
+                nextLabel="Next"
+                previousLabel="Previous"
+                breakLabel=""
+                pageCount={filmsList?.results ? 500 : 0} // The API only gives access to the first 500 Popular films pages
+                onPageChange={e => setPage(Number(e.selected)+1)}
+                containerClassName="pagination_container"
+                pageLinkClassName="pagination_button"
+                previousLinkClassName="pagination_button margin"
+                nextLinkClassName="pagination_button margin"
+                activeClassName="active"
+                pageRangeDisplayed={3}
+                marginPagesDisplayed={1}
+                renderOnZeroPageCount={null}
+            />
         </main>
     )
 }
